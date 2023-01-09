fe3a0389938250f598e9564cb3c5b4260bd4f2cb
Oliver Mercer-Smail <oliver.finlay@gmail.com>
2023-01-09 11:58:55 -0500

    mapfix

--- client/src/assets/info.js
+++
@@ -1,6 +0,0 @@
-const washrooms = [
-  { name: "rosemont", lat: "45.52886", lng: "-73.599382", gendered: true, accessible: true },
-  { name: "penus", lat: "45.52866", lng: "-73.599382", gendered: false }
-];
-
-module.exports = { washrooms };
