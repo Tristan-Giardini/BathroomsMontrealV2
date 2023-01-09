c5ddb0143787cded63edfca7929411b9e44cf7a2
Tristan Giardini <tristangiardini@gmail.com>
2023-01-09 14:15:30 -0500

    yarn added mapbox gl

--- client/src/MapContainer.js
+++ client/src/MapContainer.js
@@ -1,6 +1,5 @@
 import ReactMapBox, { Marker, GeolocateControl } from "react-map-gl";
 // eslint-disable-next-line import/no-webpack-loader-syntax
-// @ts-ignore
 mapboxgl.workerClass =
   require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 import styled from "styled-components";
