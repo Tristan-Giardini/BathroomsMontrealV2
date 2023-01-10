643586b1fa34fbe0069fd5be1d33332f3c5b2fd0
Tristan Giardini <tristangiardini@gmail.com>
2023-01-09 21:37:22 -0500

    Trying with "*" in origin

--- server/server.js
+++ server/server.js
@@ -33,21 +33,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/", express.static(__dirname + "/"));
 app.use(
   cors({
-    origin: [
-      "https://bathroomsmontreal.onrender.com, https://www.bathroomsmtl.ca, https://bathroomsmtl.ca, http://bathroomsmtl.ca",
-    ], //below added more to see if would work
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Origin",
-      "X-Requested-With",
-      "Accept",
-      "x-client-key",
-      "x-client-token",
-      "x-client-secret",
-      "Authorization",
-    ],
-    credentials: true,
+    origin: ["*"],
   })
 );
 app.use(express.static("./server/assets"));
